dc35efa7dc627fa45fd3697827642e4b7025f4d8
Omollo Oganda <cliffosego2000@gmail.com>
2024-03-30 12:41:14 +0300

    added profile options popover

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -18,17 +18,26 @@
             <i class="pi pi-video"></i>
             <i class="pi pi-bell"></i>
             <div>
-                <img alt="tania andrew"
+                <div class="relative">
+                    <img alt="tania andrew"
+                    @click="onClick"
                     src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80"
                     class="relative inline-block h-12 w-12 cursor-pointer rounded-full object-cover object-center"
                     data-popover-target="profile-menu" />
+                    <ProfileOptions class="absolute right-16 top-4" v-if="toggle"/>
+                </div>
+               
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
+    const toggle = ref(false)
 
+    const onClick = () => {
+        toggle.value = !toggle.value
+    }
 </script>
 
 <style lang="scss" scoped></style>
---
+++ components/ProfileOptions.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="bg-white w-[330px] h-screen text-sm p-3 border-gray-300 border-2 rounded-xl z-10">
+        <div class="flex gap-4 py-3">
+            <img alt="tania andrew"
+                src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80"
+                class="relative inline-block h-12 w-12 cursor-pointer rounded-full object-cover object-center"
+                data-popover-target="profile-menu" />
+            <div class="flex flex-col">
+                <h1>Osego Codes</h1>
+                <p>@osegocodes</p>
+                <a href="" class="text-blue-500">View your channel</a>
+            </div>
+        </div>
+        <hr>
+        <div class=" py-4">
+            <div class="flex gap-3 items-center py-2 px-3 my-1 mr-1 rounded-lg hover:bg-slate-100">
+                <i class="pi pi-google"></i>
+                <span>Google account</span>
+            </div>
+            <div class="flex gap-3 items-center py-2 px-3 my-1 mr-1 rounded-lg hover:bg-slate-100">
+                <i class="pi pi-users"></i>
+                <span>Switch account</span>
+            </div>
+            <div class="flex gap-3 items-center py-2 px-3 my-1 mr-1 rounded-lg hover:bg-slate-100">
+                <i class="pi pi-sign-out"></i>
+                <span>Sign out</span>
+            </div>
+            <hr class="my-1">
+            <div class="flex gap-3 items-center py-2 px-3 my-1 mr-1 rounded-lg hover:bg-slate-100">
+                <i class="pi pi-ticket"></i>
+                <span>Youtube studio</span>
+            </div>
+            <div class="flex gap-3 items-center py-2 px-3 my-1 mr-1 rounded-lg hover:bg-slate-100">
+                <i class="pi pi-money-bill"></i>
+                <span>Purchases and membership</span>
+            </div>
+            <hr class="my-1">
+            <div class="flex gap-3 items-center py-2 px-3 my-1 mr-1 rounded-lg hover:bg-slate-100">
+                <i class="pi pi-shield"></i>
+                <span>Your data in Youtube</span>
+            </div>
+            <div class="flex gap-3 items-center py-2 px-3 my-1 mr-1 rounded-lg hover:bg-slate-100">
+                <i class="pi pi-moon"></i>
+                <span>Appearance: Light</span>
+            </div>
+            <div class="flex gap-3 items-center py-2 px-3 my-1 mr-1 rounded-lg hover:bg-slate-100">
+                <i class="pi pi-key"></i>
+                <span>Language English</span>
+            </div>
+            <div class="flex gap-3 items-center py-2 px-3 my-1 mr-1 rounded-lg hover:bg-slate-100">
+                <i class="pi pi-lock"></i>
+                <span>Restricted mode: Off account</span>
+            </div>
+            <div class="flex gap-3 items-center py-2 px-3 my-1 mr-1 rounded-lg hover:bg-slate-100">
+                <i class="pi pi-globe"></i>
+                <span>Location: Kenya</span>
+            </div>
+            <div class="flex gap-3 items-center py-2 px-3 my-1 mr-1 rounded-lg hover:bg-slate-100">
+                <i class="pi pi-th-large"></i>
+                <span>Keyboard shortcuts</span>
+            </div>
+            <hr class="my-1">
+            <div class="flex gap-3 items-center py-2 px-3 my-1 mr-1 rounded-lg hover:bg-slate-100">
+                <i class="pi pi-cog"></i>
+                <span>Settings</span>
+            </div>
+            
+            
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
